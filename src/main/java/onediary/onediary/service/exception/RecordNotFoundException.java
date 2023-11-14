b52ab5e6b324d546fa548f3b690888baaf2c721b
nyeroni <nir2y@naver.com>
2023-11-14 20:40:02 +0900

    Feat: 예외 클래스 생성

---
+++ src/main/java/onediary/onediary/service/exception/RecordNotFoundException.java
@@ -0,0 +1,11 @@
+package onediary.onediary.service.exception;
+
+public class RecordNotFoundException extends RuntimeException{
+    public RecordNotFoundException(String message) {
+        super(message);
+    }
+
+    public RecordNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
