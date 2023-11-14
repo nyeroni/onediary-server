5fb38859d8a55654bf05325ece01490f06090532
nyeroni <nir2y@naver.com>
2023-11-14 11:23:45 +0900

    Initial commit

---
+++ src/test/java/onediary/onediary/OnediaryApplicationTests.java
@@ -0,0 +1,13 @@
+package onediary.onediary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnediaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
