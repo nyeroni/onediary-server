a1f4231e1ba0a5c28eaeb8f9637bfed124bac5ba
nyeroni <nir2y@naver.com>
2023-11-14 21:00:52 +0900

    Update: repository 수정

--- src/main/java/onediary/onediary/domain/member/MemberRepository.java
+++ src/main/java/onediary/onediary/domain/member/MemberRepository.java
@@ -1,9 +1,13 @@
 package onediary.onediary.domain.member;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface MemberRepository  extends JpaRepository<Member,Long > {
-    Optional<Member> findByEmail(String email);
+    @Query("SELECT m FROM Member m WHERE m.email = :email")
+    Optional<Member> findByEmail(@Param("email") String email);
+
 }
