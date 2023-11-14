64ad9b48194f2f5ed41d8e104258d61392a4492d
nyeroni <nir2y@naver.com>
2023-11-14 17:47:04 +0900

    Update: Service 구현

--- src/main/java/onediary/onediary/service/IRecordService.java
+++ src/main/java/onediary/onediary/service/IRecordService.java
@@ -1,6 +1,17 @@
 package onediary.onediary.service;
 
+import onediary.onediary.dto.record.record.RecordDto;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
 public interface IRecordService {
+    List<RecordDto> findAllRecords(String email);
+    Optional<RecordDto> findRecordById(Long recordId);
+    Optional<RecordDto> findRecordByCreatedDate(LocalDateTime createdDate);
+    RecordDto createRecord(RecordDto recordDto);
+    RecordDto updateRecordDescription(Long recordId, String updateDescription);
+    void deleteRecord(Long recordId);
 }
--- src/main/java/onediary/onediary/service/impl/MemberServiceImpl.java
+++ src/main/java/onediary/onediary/service/impl/MemberServiceImpl.java
@@ -2,6 +2,7 @@ package onediary.onediary.service.impl;
 
 import org.springframework.stereotype.Service;
 
+
 @Service
 public class MemberServiceImpl {
 }
