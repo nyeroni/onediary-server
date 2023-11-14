468c7a641757ededb3f2d877899f8787c2bc3684
nyeroni <nir2y@naver.com>
2023-11-14 19:46:08 +0900

    Update: Service 구현

--- src/main/java/onediary/onediary/service/IMemberService.java
+++ src/main/java/onediary/onediary/service/IMemberService.java
@@ -1,9 +1,10 @@
 package onediary.onediary.service;
 
 import onediary.onediary.dto.member.MemberDto;
-import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
 public interface IMemberService {
+    Optional<MemberDto> findMemberById(Long memberId);
+    Optional<MemberDto> findByEmail(String email);
 }
--- src/main/java/onediary/onediary/service/impl/MemberServiceImpl.java
+++ src/main/java/onediary/onediary/service/impl/MemberServiceImpl.java
@@ -1,7 +1,31 @@
 package onediary.onediary.service.impl;
 
+import lombok.RequiredArgsConstructor;
+import onediary.onediary.domain.member.Member;
+import onediary.onediary.domain.member.MemberRepository;
+import onediary.onediary.dto.member.MemberDto;
+import onediary.onediary.service.IMemberService;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
+@RequiredArgsConstructor
 @Service
-public class MemberServiceImpl {
+public class MemberServiceImpl implements IMemberService {
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    public Optional<MemberDto> findMemberById(Long memberId){
+        Optional<Member> optionalMember =  memberRepository.findById(memberId);
+        return optionalMember.map(MemberDto::toDto);
+    }
+
+    @Override
+    public Optional<MemberDto> findByEmail(String email){
+        Optional<Member> optionalMember = memberRepository.findByEmail(email);
+        return optionalMember.map(MemberDto::toDto);
+    }
+
+
 }
