49ee26a69fce3117bc0dd6043dae85990b2f04c2
nyeroni <nir2y@naver.com>
2023-11-14 20:39:39 +0900

    Update: Service 구현

--- src/main/java/onediary/onediary/domain/record/RecordRepository.java
+++ src/main/java/onediary/onediary/domain/record/RecordRepository.java
@@ -1,6 +1,16 @@
 package onediary.onediary.domain.record;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 
 public interface RecordRepository extends JpaRepository<Record, Long> {
+    @Query("SELECT r FROM Record r WHERE r.member.email = :email")
+    List<Record> findAllByEmail(@Param("email") String email);
+    @Query("SELECT r FROM Record r WHERE DATE(r.createdDate) = DATE(:createdDate)")
+    Optional<Record> findByDate(@Param("createdDate") LocalDateTime createdDate);
 }
