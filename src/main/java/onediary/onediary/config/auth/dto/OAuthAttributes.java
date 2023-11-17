aad2cf778846c13e4ee37457126a2969a1cc2e96
nyeroni <nir2y@naver.com>
2023-11-18 01:15:43 +0900

    Feat: 네이버 소셜로그인 추가

--- src/main/java/onediary/onediary/config/auth/dto/OAuthAttributes.java
+++ src/main/java/onediary/onediary/config/auth/dto/OAuthAttributes.java
@@ -21,16 +21,15 @@ public class OAuthAttributes {
     private String nameAttributeKey;
     private String name;
     private String email;
-
+    private SocialProvider socialProvider;
 
     public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes){
         switch (registrationId) {
-//            case "google":
-//                return ofGoogle(attributeKey, attributes);
+
             case "kakao":
                 return ofKakao("email", attributes);
-//            case "naver":
-//                return ofNaver("id", attributes);
+            case "naver":
+                return ofNaver("id", attributes);
             default:
                 throw new RuntimeException();
         }
@@ -47,11 +46,23 @@ public class OAuthAttributes {
                 .name((String) kakaoProfile.get("nickname"))
                 .build();
     }
+    private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes){
+        attributes.put("socialProvider", SocialProvider.NAVER);
+        Map<String, Object> naverAccount = (Map<String, Object>) attributes.get("naver_account");
+        Map<String, Object> naverProfile = (Map<String, Object>) naverAccount.get("profile");
+
+        return OAuthAttributes.builder()
+                .attributes(naverAccount)
+                .nameAttributeKey(userNameAttributeName)
+                .email((String) naverAccount.get("email"))
+                .name((String) naverProfile.get("name"))
+                .build();
+    }
     public Member toEntity(){
         return Member.builder()
                 .username(name)
                 .email(email)
-                .socialProvider(SocialProvider.KAKAO)
+                .socialProvider(socialProvider)
                 .build();
     }
 
