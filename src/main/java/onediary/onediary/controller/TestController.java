3e3ef97080e3a841c861808a0d3ad52023face04
nyeroni <nir2y@naver.com>
2023-11-17 17:02:25 +0900

    Update: API TEST

--- src/main/java/onediary/onediary/controller/TestController.java
+++ src/main/java/onediary/onediary/controller/TestController.java
@@ -4,13 +4,11 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api")
 @Slf4j
 public class TestController {
     @GetMapping("/success")
@@ -21,6 +19,6 @@ public class TestController {
     @PostMapping("/success")
     public ResponseEntity postSuccessMessage(@RequestParam String param){
         log.info("param = {}", param);
-        return new ResponseEntity<>("success", HttpStatus.OK);
+        return new ResponseEntity<>("param", HttpStatus.OK);
     }
 }
