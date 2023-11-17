1738e160be5ae160e7a734bbf5abeb886efbb18b
nyeroni <nir2y@naver.com>
2023-11-17 15:01:05 +0900

    Update: @EnableJpaAuditing 어노테이션 추가

--- src/main/java/onediary/onediary/OnediaryApplication.java
+++ src/main/java/onediary/onediary/OnediaryApplication.java
@@ -2,8 +2,10 @@ package onediary.onediary;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class OnediaryApplication {
 
 	public static void main(String[] args) {
