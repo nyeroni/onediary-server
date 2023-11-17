44fce9119322104045475c19d0d435f58791fbdb
nyeroni <nir2y@naver.com>
2023-11-17 15:05:36 +0900

    Update: 서비스 구현 및 수정

--- src/main/java/onediary/onediary/service/impl/RecordServiceImpl.java
+++ src/main/java/onediary/onediary/service/impl/RecordServiceImpl.java
@@ -5,14 +5,18 @@ import onediary.onediary.domain.member.Member;
 import onediary.onediary.domain.member.MemberRepository;
 import onediary.onediary.domain.record.Record;
 import onediary.onediary.domain.record.RecordRepository;
-import onediary.onediary.dto.record.record.RecordDto;
+import onediary.onediary.dto.record.record.RecordViewDto;
+import onediary.onediary.dto.record.record.RecordWriteDto;
 import onediary.onediary.service.IRecordService;
 import onediary.onediary.service.exception.RecordNotFoundException;
+import org.hibernate.Hibernate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
@@ -22,65 +26,80 @@ public class RecordServiceImpl implements IRecordService {
     private final RecordRepository recordRepository;
     private final MemberRepository memberRepository;
     @Override
-    public List<RecordDto> findAllRecords(String email){
+    @Transactional
+    public List<RecordViewDto> getRecords(Long memberId){
+        Member member = memberRepository.findById(memberId).get();
+        List<Record> recordList = recordRepository.findAllByEmail(member.getEmail());
+
+        List<RecordViewDto> recordViewDtos = new ArrayList<>();
+        for(Record record : recordList){
+            recordViewDtos.add(RecordViewDto.toDto(record));
+        }
+        return recordViewDtos;
+    }
+    @Override
+    @Transactional(readOnly = true)
+    public List<RecordViewDto> findAllRecords(String email){
+        List<Record> recordList = recordRepository.findAllByEmail(email);
+        return recordList.stream().map(RecordViewDto::toDto).collect(Collectors.toList());
+    }
+    @Transactional(readOnly = true)
+    @Override
+    public RecordViewDto findRecordById(Long recordId) {
+        Optional<Record> optionalRecord = recordRepository.findById(recordId);
+
+        return optionalRecord.map(RecordViewDto::toDto).orElse(null);
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public Optional<Long> findRecordByCreatedDate(LocalDateTime createdDate) {
+        System.out.println("createdDate="+createdDate);
+        Optional<Record> recordOptional = recordRepository.findByCreatedDate(createdDate);
+
+        return recordOptional.map(Record::getId);
+    }
+    @Transactional
+    @Override
+    public Long createRecord(RecordWriteDto recordDto){
+        String email = recordDto.getEmail();
         Optional<Member> optionalMember = memberRepository.findByEmail(email);
+        Member member = optionalMember.orElseThrow(() -> new NoSuchElementException("Member not found with email: " + email));
 
-        if(optionalMember.isPresent()){
-            List<Record> allRecords = recordRepository.findAllByEmail(email);
+        Record record = recordDto.toEntity(member);
+        member.addRecord(record);
+        recordRepository.save(record);
 
-            return allRecords.stream()
-                    .map(RecordDto::toDto)
-                    .collect(Collectors.toList());
-        }
-        else{
-            return Collections.emptyList();
-        }
-
+        return record.getId();
     }
+    @Transactional
     @Override
-    public Optional<RecordDto> findRecordById(Long recordId){
-        Optional<Record> optionalRecord = recordRepository.findById(recordId);
-        return optionalRecord.map(RecordDto::toDto);
-    }
-    @Override
-    public Optional<RecordDto> findRecordByCreatedDate(LocalDateTime createdDate){
-        Optional<Record> optionalRecord = recordRepository.findByDate(createdDate);
-        return optionalRecord.map(RecordDto::toDto);
-    }
-    @Override
-    public RecordDto createRecord(RecordDto recordDto){
-        Record record = recordDto.toEntity();
+    public Long updateRecordDescription(Long recordId, String updatedDescription){
+        Record record = recordRepository.findById(recordId)
+                .orElseThrow(() -> new IllegalArgumentException("해당 일기가 없습니다. id=" + recordId));
+        System.out.println("record==="+record.getDescription());
 
-        Record savedRecord = recordRepository.save(record);
-        return RecordDto.toDto(savedRecord);
-    }
-    @Override
-    public RecordDto updateRecordDescription(Long recordId, String updatedDescription){
-        Optional<Record> optionalRecord = recordRepository.findById(recordId);
+        record.update(updatedDescription);
+        System.out.println("record==="+record.getDescription());
 
-        if(optionalRecord.isPresent()){
-            Record record = optionalRecord.get();
-            Record updateRecord = Record.builder()
-                    .id(record.getId())
-                    .description(updatedDescription)
-                    .member(record.getMember())
-                    .build();
-            Record saveRecord = recordRepository.save(updateRecord);
-            return RecordDto.toDto(saveRecord);
 
-        }
-        else{
-            return null;
-        }
+        return recordId;
+
     }
+    @Transactional
     @Override
     public void deleteRecord(Long recordId){
         Optional<Record> optionalRecord = recordRepository.findById(recordId);
-        optionalRecord.ifPresent(record -> {
-            recordRepository.delete(record);
-        });
+
         if (!optionalRecord.isPresent()) {
             throw new RecordNotFoundException("Record not found with id: " + recordId);
         }
+        optionalRecord.ifPresent(record -> {
+            recordRepository.delete(record);
+        });
+    }
+    @Override
+    public List<RecordViewDto> getRecordsAfterUpdate(Long memberId) {
+        return getRecords(memberId);
     }
 }
