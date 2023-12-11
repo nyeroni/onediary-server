01ec3c4527cecd214042f22690d020ed1e6c18ac
nyeroni <nir2y@naver.com>
2023-12-11 10:56:29 +0900

    Update: cors 관련 수정

--- src/test/java/onediary/onediary/AuthControllerTest.java
+++ src/test/java/onediary/onediary/AuthControllerTest.java
@@ -1,8 +1,6 @@
 package onediary.onediary;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import onediary.onediary.oauth.controller.AuthController;
-import onediary.onediary.oauth.service.KakaoOauthService;
 import onediary.onediary.oauth.token.AuthResponseDto;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,8 +13,6 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.transaction.annotation.Transactional;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -41,19 +37,17 @@ public class AuthControllerTest {
     public void 카카오_회원가입_및_로그인_성공() throws Exception{
 
         //given
-        KakaoOauthService mockKakaoOauthService = mock(KakaoOauthService.class);
-        when(mockKakaoOauthService.login(accessToken))
-                .thenReturn(AuthResponseDto.builder()
-                        .jwtToken(accessToken)
-                        .isNewMember(false)
-                        .build());
+        String object = objectMapper.writeValueAsString(AuthResponseDto.builder()
+                .jwtToken(accessToken)
+                .isNewMember(false)
+                .build());
 
         //when
-        AuthController authController = new AuthController(mockKakaoOauthService, null, null);
         ResultActions actions = mockMvc.perform(
                 post("/auth/kakao")
                         .contentType(MediaType.APPLICATION_JSON)
                         .accept(MediaType.APPLICATION_JSON)
+                        .content(object)
         );
 
         //then
