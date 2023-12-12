525beafad5bb782c536941e40bf98d0d70aca484
nyeroni <nir2y@naver.com>
2023-12-13 01:03:50 +0900

    Update: controller 수정

--- src/main/java/onediary/onediary/oauth/controller/AuthController.java
+++ src/main/java/onediary/onediary/oauth/controller/AuthController.java
@@ -1,7 +1,6 @@
 package onediary.onediary.oauth.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import onediary.onediary.common.apiResponse.ApiResponse;
@@ -13,7 +12,10 @@ import onediary.onediary.oauth.token.AuthToken;
 import onediary.onediary.oauth.token.AuthTokenProvider;
 import onediary.onediary.oauth.utils.JwtHeaderUtil;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
@@ -31,19 +33,22 @@ public class AuthController {
      */
     @Operation(summary = "카카오 로그인", description = "카카오 엑세스 토큰을 이용하여 사용자 정보 받아 저장하고 앱의 토큰 반환")
     @PostMapping(value = "/login/kakao", produces = "application/json")
-    public ResponseEntity<AuthResponseDto> kakaoAuthRequest(@RequestBody AuthRequestDto authRequest) {
-        return ApiResponse.success(kakaoAuthService.login(authRequest));
-    }
+    public ResponseEntity<AuthResponseDto> kakaoAuthRequest(@RequestHeader("Authorization") String authHeader) {
+        String token = JwtHeaderUtil.extractAccessTokenFromHeader(authHeader);
 
+        AuthRequestDto authRequestDto = new AuthRequestDto(token);
+        return ApiResponse.success(kakaoAuthService.login(authRequestDto));
+    }
 
     /**
      * appToken 갱신
      * @return ResponseEntity<AuthResponse>
      */
     @Operation(summary = "jwtToken 갱신", description = "jwtToken 갱신")
-    @GetMapping(value = "/refresh", produces = "application/json")
-    public ResponseEntity<AuthResponseDto> refreshToken (HttpServletRequest request) {
-        String appToken = JwtHeaderUtil.getAccessToken(request);
+    @PostMapping(value = "/refresh", produces = "application/json")
+    public ResponseEntity<AuthResponseDto> refreshToken (@RequestHeader("Authorization") String authHeader) {
+
+        String appToken = JwtHeaderUtil.extractAccessTokenFromHeader(authHeader);
         AuthToken authToken = authTokenProvider.convertAuthToken(appToken);
         if (!authToken.validate()) { // 형식에 맞지 않는 token
             return ApiResponse.forbidden(null);
